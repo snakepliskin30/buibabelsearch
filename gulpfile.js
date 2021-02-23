a3188f3cb6c3046e8bc6b6a27990e34bef0a9ffc
Rod <rodtolaresa@gmail.com>
2021-02-23 23:44:33 +0800

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 const gulp = require("gulp");
 const zip = require("gulp-zip");
+const inject = require("gulp-inject-string");
 
-exports.default = () =>
+gulp.task("insert", function (done) {
+  gulp
+    .src("./dist/index-*.html")
+    .pipe(
+      inject.before(
+        "<title>",
+        '<script defer="defer" src="../vendorlib/vendor.js"></script>'
+      )
+    )
+    .pipe(gulp.dest("./dist/"));
+  console.log("Insert task done!");
+  done();
+});
+
+gulp.task("zip", function (done) {
   gulp.src("dist/**/*.*").pipe(zip("app.zip")).pipe(gulp.dest("."));
+  console.log("Zip task done");
+  done();
+});
